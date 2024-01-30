756498b55949eeed7d8a269d88c371d5163b43dc
dezlymacauley <dezlymacauley@proton.me>
2024-01-30 15:49:35 +0200

    Added an if else flow to the switch statement

--- 13_case_statements.cpp
+++ 13_case_statements.cpp
@@ -11,12 +11,11 @@ int main() {
     cin >> health_bar;
 
     switch (health_bar) {
-        
-        // NOTE: This means 10 or 15 or 20
-        // A message will be displayed whenever health hits these exact levels.
         case 100:
             cout << "You health is at max" << endl;
             break;
+        // NOTE: This means 10 or 15 or 20
+        // A message will be displayed whenever health hits these exact levels.
         case 10: 
         case 15:
         case 20:
@@ -25,13 +24,29 @@ int main() {
         case 50:
             cout << "Protective shield has been lost" << endl;
             break;
+        case 0:{
+            
+            // NOTE: If you are declaring a variable in the case statement,
+            // then don't forget to add the {}
+            
+            char give_up {'N'};
+            cout << "Ouch! Do you want to give up (Y/N): ";
+            cin >> give_up;
 
+            if (give_up == 'Y') {
+                cout << "Thanks for playing" <<endl;
+            } else if (give_up == 'N') {
+                cout << "Let's get you back into the fight" <<endl;
+            } else {
+                cout << "Too dizzy to respond properly huh." <<endl;
+            }
+
+            break;
+        }
         // NOTE: This works like a final `else` statement
         default:
             cout << "You are still in this fight" << endl;
     }
 
-
-    
 
 }
