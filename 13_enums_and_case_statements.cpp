e0a32ded88977176b6e2cb4d45fd841a26da2dd6
dezlymacauley <dezlymacauley@proton.me>
2024-01-30 16:25:29 +0200

    Added section on enums

---
+++ 13_enums_and_case_statements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    
+    // NOTE: First define the enum
+    enum Direction {
+        left, right, up, down
+    };
+    
+    // NOTE: Then create a variable based on the enum
+    Direction player_movement {left};
+
+    switch (player_movement) {
+        case left:
+            cout << "Player is moving left" << endl;
+        case right:
+            cout << "Player is moving right" << endl;
+        default:
+            cout << "Player is moving" << endl;
+    }
+    
+    return 0;
+}
