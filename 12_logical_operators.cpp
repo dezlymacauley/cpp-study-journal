44b26db3d437b0c530088936759aaa7c6b0a541e
dezlymacauley <dezlymacauley@proton.me>
2024-01-30 07:07:49 +0200

    Learned logical and relational operators

---
+++ 12_logical_operators.cpp
@@ -0,0 +1,35 @@
+/*
+
+! means not
+&& means and
+|| means or
+
+NOTE: not has higher precedence than and
+and has higher precedence than or
+
+NOTE: `not` is a unary operator
+and and or are binary operators
+
+NOTE: Short-Circuit Evaluation
+When evaluation a logical expression, C++ stops as soon as the result is known.
+
+===============================================================================
+
+NOTE: Compound Assignment Operators
+
++=          `a += 1;` is the same as `a = a + 1;` 
+-=          `a -= 1;` is the same as `a = a - 1;`
+/=          `a /= 5;` is the same as `a = a / 5;`
+*=          `a *= b + c` is the same as `a = a * (b + c)`
+%=
+>>=
+<<=
+&=
+^=
+|=
+
+cost += items * tax; same as `cost = cost + (items * tax);`
+
+===============================================================================
+
+*/
---
+++ 12_relational_operators.cpp
@@ -0,0 +1,30 @@
+/*
+
+NOTE: Relational Operators
+
+= Equals
+=! Not equals
+
+> greater than
+>= greater than or equal to
+< less than
+> greater than
+
+NOTE: 
+<=> three-way comparision (C++20)
+This evaluates three expressions.
+    result == 0: The two values are equal.
+    result < 0: The left operand is less than the right operand.
+    result > 0: The left operand is greater than the right operand.
+
+It's important to note that the actual values of the positive, 
+or negative result are implementation-defined and may vary,
+depending on the specific type being compared. 
+
+The standard library provides types like,
+std::strong_ordering, std::weak_ordering, and std::partial_ordering,
+to represent the result of the comparison,
+in a more type-safe and expressive manner.
+
+
+*/
