af550482e55875fd3b617a748ff41ed5b93be589
dezlymacauley <dezlymacauley@proton.me>
2024-01-31 15:40:03 +0200

    Added the basic structure

--- 15_passing_arrays_to_functions.cpp
+++ 15_passing_arrays_to_functions.cpp
@@ -15,7 +15,7 @@ void print_array(int _array, size_t _number_of_vales_in_array);
 
 // This will replace all of the values in an array with a replacement number.
 void replace_numbers_in_array(
-    int _array, 
+    int _array[], 
     size_t _number_of_vales_in_array, 
     int _replacement_number
 );
@@ -30,13 +30,33 @@ the number of elements in a container, or the result of the sizeof operator.
 
 */
 
+// NOTE: Write the function logic
+// _i is short for index
+void print_array(int _array[], size_t _number_of_vales_in_array) {
+    for (size_t _i{0}; _i < _number_of_vales_in_array; ++_i) {
+        cout << _array[_i] << " ";
+    };
+    cout << endl;
+}
 
+void replace_numbers_in_array(
+    int _array[], 
+    size_t _number_of_vales_in_array, 
+    int _replacement_number
+) {
 
+   for (size_t _i; _i < _number_of_vales_in_array; ++_i) {
+        _array[_i] = _replacement_number; 
+    };
+
+}
 
 int main() {
 
     int my_scores[] {100,98,90,86,84};
-
+    
+    print_array(my_scores,5);
+    replace_numbers_in_array(my_scores,5, 9);
 
     cout << endl;
     return 0;
